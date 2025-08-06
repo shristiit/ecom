dda0ae7f0a0c5ab2fbcc186060f1bfc27f2a1f12
Tharun sai <tharunsai@Tharuns-MacBook-Air.local>
2025-08-06 19:24:14 +0100

    product page updated

--- admin/src/app/Assets/ProductData.ts
+++ admin/src/app/Assets/ProductData.ts
@@ -10,3 +10,20 @@ export const ProductCategory =[
   "Yasmin",
 ];
 
+export const SeasonOptions = [
+  "AW10", "AW11", "AW12", "AW13", "AW14", "AW15", "AW16", "AW17", "AW18",
+  "SS10", "SS11", "SS12", "SS13", "SS14", "SS15", "SS16", "SS17", "SS18", "SS19", "SS20"
+];
+
+export const BrandOptions = [
+  "Annika",
+  "Dynasty",
+  "Dynasty Bridal",
+  "Dynasty Cocktail",
+  "Dynasty Curve",
+  "Dynasty Krystal London",
+  "Dynasty Premium",
+  "Dynasty Spirit",
+  "Viviana",
+  "Yasmin"
+]; 
