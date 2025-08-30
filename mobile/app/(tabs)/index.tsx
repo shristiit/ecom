f482fd23450e94acccab521be9cd4693908caf38
tharunsai4 <tharunsai461@gmail.com>
2025-08-30 10:31:44 +0100

    test commit

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
-
+import { Text } from '@react-navigation/elements';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
